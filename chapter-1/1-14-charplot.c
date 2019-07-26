e8ff109465864a829f9830b66141d35c0da68baa
gk07 <gk_520@hotmail.com>
2019-07-26 17:49:42 +0800

    Update 1-14-charplot, incorporated horizontal and vertical plot

--- chapter-1/1-14-charplot.c
+++ chapter-1/1-14-charplot.c
@@ -4,20 +4,31 @@
 #define LOWER   '!'
 #define PLOTSYM '#'
 
-void plotHorizontal(void);
-void plotVertical(void);
+void plotHorizontal(int *);
+void plotVertical(int *);
+int * charCounter(void);
+int max(int *);
 
+int charc[RANGE];
 
 int main(int argc, char const *argv[])
 {
-    plotHorizontal();
-    plotVertical();
+    int * charc;
+
+    charc = charCounter();
+    printf("==========================================\n"); 
+    printf("The Horizontal Plot\n");
+    printf("==========================================\n"); 
+    plotHorizontal(charc);
+    printf("==========================================\n"); 
+    printf("The Vertical Plot\n");
+    printf("==========================================\n"); 
+    plotVertical(charc);
 }
 
-void plotHorizontal(void)
+int* charCounter(void)
 {
     int c, i, j;
-    int charc[RANGE];
 
     for(i = 0; i < RANGE; ++i)
         charc[i] = 0;
@@ -27,7 +38,14 @@ void plotHorizontal(void)
         if(c >= '!' && c <= '~')
             ++charc[c-LOWER];
     }
-    putchar('\n');
+
+    return charc;
+}
+
+void plotHorizontal(int *charc)
+{
+    int i, j;
+
     for(i = 0; i < RANGE; ++i)
     {
         if(charc[i] > 0)
@@ -41,7 +59,82 @@ void plotHorizontal(void)
     }
 }
 
-void plotVertical(void)
+int max(int *arr)
 {
+    int i, m;
+    m = 0;
 
+    for(i = 0; i < RANGE; ++i)
+    {
+        if(arr[i] > m)
+            m = arr[i];
+    }
+
+    return m;
+}
+
+void plotVertical(int *charc)
+{
+    int maxHeight, i, j, row;
+    int charcCopy[RANGE];
+    maxHeight = max(charc);
+
+    int rows[maxHeight][RANGE];
+
+    // make a copy of charc list
+    for (i = 0; i < RANGE; i++)
+    {
+        charcCopy[i] = charc[i];
+    }
+
+    // store charcounter list in a 2d array
+    for (i = 0; i < maxHeight; i++)
+    {
+        for (j = 0; j < RANGE; j++)
+        {
+            if(charc[j] > 0)
+            {
+                rows[i][j] = 1;
+                charc[j] = charc[j] - 1;
+            }
+            else
+            {
+                rows[i][j] = 0;
+            }
+        }
+    }
+
+    // print the vertical plot, drop the empty colnums
+    for(row = maxHeight - 1; row >= 0; row--)
+    {
+        int i;
+
+        for (i = 0; i < RANGE; i++)
+        {
+            if(charcCopy[i] > 0){
+                if(rows[row][i] > 0)
+                {
+                    putchar(PLOTSYM);
+                    putchar(' ');
+                }
+                else
+                {
+                    putchar(' ');
+                    putchar(' ');
+                }
+            }
+        }
+        putchar('\n');
+    }
+
+    // print the chars as x-axis
+    for (i = 0; i < RANGE; i++)
+    {
+        if(charcCopy[i] > 0)
+        {
+            putchar(i + LOWER);
+            putchar(' ');
+        }
+    }
+    putchar('\n');
 }
